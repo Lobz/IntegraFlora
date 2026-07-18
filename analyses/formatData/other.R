f77861b82f7d59e6644d45c45764b91513c5c97a
Mali Oz <malicsalles@gmail.com>
2026-07-18 10:19:40 -0300

    allow other source files to have 0 rows

--- analyses/formatData/other.R
+++ analyses/formatData/other.R
@@ -3,13 +3,15 @@ if(!require(integraFlora)) devtools::load_all()
 require(plantR)
 
 # other data
-other_files <- list.files("data-input/Occurrences/OtherSources", pattern = "*.csv", full.names = TRUE, recursive = TRUE)
+other_files <- list.files("data-input/Occurrences/OtherSources", pattern = "*.(csv|txt)", full.names = TRUE, recursive = TRUE)
 if(length(other_files) > 0) {
     print("Reading other files:")
     print(other_files)
     other_data_raw <- lapply(other_files, readOccurrence)
+    sizes <- sapply(other_data_raw, nrow)
+    other_data_raw_positive <- other_data_raw[sizes > 0]
     print("Parsing other data...")
-    other <- lapply(other_data_raw, formatOccurrence)
+    other <- lapply(other_data_raw_positive, formatOccurrence)
 } else {
     other <-list()
 }
