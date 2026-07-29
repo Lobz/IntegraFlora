7e1e31a1562469fd2281b405fbadd3ed10b12e72
Mali Oz <malicsalles@gmail.com>
2026-07-29 10:45:34 -0300

    install dep packages on debian/ubuntu

---
+++ analyses/install_deps_linux.sh
@@ -0,0 +1,2 @@
+apt install libssl-dev libcurl4-openssl-dev libfontconfig1-dev libxml2-dev libuv1-dev \
+     libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libwebp-dev
