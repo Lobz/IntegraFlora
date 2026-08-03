0d2de5e566b0e1df3e574f5f7a853c7fcf1a2842
Mali Oz <malicsalles@gmail.com>
2026-08-03 15:47:27 -0300

    fix bugs

--- downloadSpLink.sh
+++ downloadSpLink.sh
@@ -1,13 +1,18 @@
+#1/bin/bash
 export SP_URL="https://specieslink.net/search/download-gzip/20260801142304-0031534"
 export N_PARTS=4
 
 cd data-input/Occurrences/splink/
-mkdir parts
+if [ ! -d parts ]; then
+    mkdir parts
+fi
 cd parts
 for i in $(seq $N_PARTS)
 do
-    wget $SP_URL-$i
+    wget -O splink-part-$i.txt.gz $SP_URL-$i
+    gzip --decompress splink-part-$i.txt.gz
 done
-gzip --decompress *
 cd ..
-cat parts/* > splink_large_file.txt
+cat parts/*.txt > large-file.txt
+
+cd ../../../
