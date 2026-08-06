aec67e618312758e1080f19df5f70693ff4164a8
Mali Oz <malicsalles@gmail.com>
2026-08-06 16:00:26 -0300

    load config.R in createUCsummary

--- analyses/createUCsummary.R
+++ analyses/createUCsummary.R
@@ -1,5 +1,7 @@
 if(!require(integraFlora)) devtools::load_all()
 require(plantR)
+source("config.R")
+
 folder <- "data-input/Locations/info/"
 info_files <- read.csv("data-input/Locations/info/file_descriptions.csv", na.strings = c("", "NA"))
 filenames <- paste0("data-input/Locations/info/", info_files$arquivo)
