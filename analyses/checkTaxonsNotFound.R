1fa1408501edf25ed1c73ffe4381ccb29dd4c15e
Mali Oz <malicsalles@gmail.com>
2026-08-13 14:36:25 -0300

    using function to encapsulate stuff

--- analyses/checkTaxonsNotFound.R
+++ analyses/checkTaxonsNotFound.R
@@ -6,45 +6,43 @@ taxons <- read.csv("results/taxons_not_found.csv")
 
 head(taxons)
 
-x <- completeScientificName(taxons)
-# For records that have authorship inside scientific name, we want to remove that
-x <- removeRepeatedAuthorship(x)
-x <- getTaxonId(x)
+fullGetId <- function(x) {
+
+    print("Formatting taxonomy...")
+    x <- completeScientificName(x, rm.miss = T)
+    x <- removeRepeatedAuthorship(x)
+
+    if(!exists("bfoNamesBryophyta")){
+        print("Loading bryophyta and algae...")
+        data(list = c("bfoNamesBryophyta", "bfoNamesAlgae"), package = "plantRdata")
+    }
+    tax <- dplyr::full_join(bfoNamesBryophyta, bfoNamesAlgae)
+    tax <- dplyr::full_join(tax, plantR::bfoNames)
+    x <- getTaxonId(x, db = tax)
+
+    x <- tryAgain(x, not_found, function(x) {
+        x <- isolateAuthorship(x)
+        x <- getTaxonId(x, db = tax)
+    })
+
+    # We'll try getting extra taxons with wfo
+    # loading the WFO and WCVP backbones
+    if(!exists("wfoNames")) {
+        print("Loadind world flora databases...")
+        data(list = c("wfoNames", "wcvpNames"), package = "plantRdata")
+    }
+    # using the World Flora Online
+    x <- tryAgain(x, not_found, getTaxonId, db = wfoNames)
+    # using the World Checklist of Vascular Plants
+    x <- tryAgain(x, not_found, getTaxonId, db = wcvpNames)
+}
+
+x <- fullGetId(taxons)
 tab(x$tax.notes)
-x <- x[not_found(x), ]
 
-data(list = c("bfoNamesBryophyta", "bfoNamesAlgae"), package = "plantRdata")
-tax <- dplyr::full_join(bfoNamesBryophyta, bfoNamesAlgae)
-# using the Bryophyta and Algae
-x <- tryAgain(x, not_found, getTaxonId, db = tax)
-
-x <- tryAgain(x, not_found, function(x) {
-    x <- isolateAuthorship(x)
-    x <- getTaxonId(x)
-    # using the Bryophyta and Algae
-    x <- tryAgain(x, not_found, getTaxonId, db = tax)
-})
-
-tab(x$tax.notes)
-
-x <- x[not_found(x),]
-head(x)
 names(x)[4] <- "Freq"
 
 write.csv(x[,1:4], "results/taxons_not_found.csv", row.names=F)
-
-# We'll try getting extra taxons with wfo
-# loading the WFO and WCVP backbones into a temporary environment
-# using the World Flora Online
-data(list = c("wfoNames", "wcvpNames"), package = "plantRdata")
-x <- tryAgain(x, not_found, getTaxonId, db = wfoNames)
-# using the World Checklist of Vascular Plants
-x <- tryAgain(x, not_found, getTaxonId, db = wcvpNames)
-
-tab(x$tax.notes)
-
-x <- x[not_found(x),]
-head(x)
 
 nf <- x[not_found(x), ]
 write.csv(nf[,1:4], "results/taxons_not_found.csv", row.names=F)
@@ -69,6 +67,10 @@ ni <- lapply(nf_lists, read.csv)
 ni <- do.call(rbind, ni)
 
 y <- reverseListFormatting(ni)
+
+y <- fullGetId(y)
+
+tab(y$tax.notes)
 
 setdiff(x$scientificName, y$scientificName)
 setdiff(y$scientificName, x$scientificName)
